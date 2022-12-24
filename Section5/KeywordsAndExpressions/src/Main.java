d97f3d253c79b406fcde0c620084fadd72bdfed7
vtallen <89719939+vtallen@users.noreply.github.com>
2022-12-24 09:24:13 -0500

    StatementsWiteSpaceAndINdenting project

--- Section5/KeywordsAndExpressions/src/Main.java
+++ Section5/KeywordsAndExpressions/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        double kilometers = (100 * 1.609344);
 
-        System.out.println("Hello world!");
+        int highScore = 50;
+
+        if (highScore > 25) {
+            highScore = 1000 + highScore;
+        }
+
+        int health = 100;
+
+        if ((health < 25) && (highScore > 100)) {
+            highScore = highScore - 1000;
+        }
+        
     }
 }
