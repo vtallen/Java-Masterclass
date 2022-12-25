bd53cf0e017aec33d849006f8e33e90d948f9304
vtallen <89719939+vtallen@users.noreply.github.com>
2022-12-24 23:14:35 -0500

    Completed Coding Exercise 3

---
+++ Section5/CodingExercise3/src/MegaBytesConverter.java
@@ -0,0 +1,21 @@
+public class MegaBytesConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        if (!(kiloBytes < 0)) {
+            int mb = (kiloBytes / 1024);
+            int kbRemainder = kiloBytes % 1024;
+
+            System.out.println(kiloBytes +
+                    " KB = " +
+                    mb + " MB and " +
+                    kbRemainder + " KB"
+            );
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+}
