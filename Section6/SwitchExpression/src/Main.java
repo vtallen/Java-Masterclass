3bcedfe0324205b92e84791691d64d77193a4800
vtallen <89719939+vtallen@users.noreply.github.com>
2022-12-26 17:14:54 -0500

    Finished SwitchChallenge

---
+++ Section6/SwitchExpression/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+    }
+
+    public static void printDayOfWeek(int day) {
+        String dayOfTheWeek = switch (day) {
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+
+        System.out.println("Day " + day + ": " + dayOfTheWeek);
+    }
+}
