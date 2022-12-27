93c3f6c2df06b804bbc5ed80ca5cb6a94ae886be
vtallen <89719939+vtallen@users.noreply.github.com>
2022-12-27 12:14:31 -0500

    Finished the PrimeNumberChallenge

---
+++ Section6/ThePrimeNumberChallenge/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("0 is " + (isPrime(0) ? "" : "NOT") + " a prime number");
+        System.out.println("1 is " + (isPrime(1) ? "" : "NOT") + " a prime number");
+        System.out.println("2 is " + (isPrime(2) ? "" : "NOT") + " a prime number");
+        System.out.println("3 is " + (isPrime(3) ? "" : "NOT") + " a prime number");
+
+        int numPrime = 0;
+        for (int i = 0; i <= 1000; i++) {
+            if (isPrime(i)) {
+                numPrime++;
+                System.out.println("Prime number found: " + i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2) {
+            return (wholeNumber == 2);
+        }
+
+        for (int divisor = 2; divisor <= wholeNumber / 2; divisor++) {
+            if (wholeNumber % divisor == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
