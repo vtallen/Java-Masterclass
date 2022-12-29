fe6efc79f9c8863ff727935141fcc9b5ae5b8885
vtallen <89719939+vtallen@users.noreply.github.com>
2022-12-28 23:37:51 -0500

    Completed the scanner lecture

--- Section6/ReadingUserInput/src/Main.java
+++ Section6/ReadingUserInput/src/Main.java
@@ -31,9 +31,33 @@ public class Main {
         System.out.println("Hi " + name);
 
         System.out.println("What year were you born? ");
-        String dateOfBirth = scanner.nextLine();
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        boolean validDOB = false;
+        int age = 0;
+        do {
+            System.out.println("Enter a year of birth >= " +
+                    (currentYear - 125) + " and <= " + (currentYear));
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDOB = age < 0 ? false : true;
+            } catch (NumberFormatException badUserData) {
+                System.out.println("Characters not allowed! Try again.");
+            }
+
+        } while (!validDOB);
+
 
         return "So you are " + age + " years old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+        int dob = Integer.parseInt(dateOfBirth);
+        int minYear = currentYear - 125;
+
+        if ((dob < minYear) || (dob > currentYear)) {
+            return -1;
+        }
+
+        return (currentYear - dob);
     }
 }
