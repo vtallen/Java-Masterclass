047beec9cd927ccd23109255a860797100ae0b1e
vtallen <89719939+vtallen@users.noreply.github.com>
2022-12-30 11:34:53 -0500

    Completed the SimpleCalculator coding exercise

---
+++ Section7/CodingExercises/src/SimpleCalculator.java
@@ -0,0 +1,45 @@
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    //Setters
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    //Getters
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return this.secondNumber;
+    }
+
+
+    //methods
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0;
+        }
+        return firstNumber / secondNumber;
+    }
+
+
+}
