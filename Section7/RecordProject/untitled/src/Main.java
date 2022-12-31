ff886a39356d1564b30fd2c5940085d7e530b0ac
vtallen <89719939+vtallen@users.noreply.github.com>
2022-12-31 15:37:28 -0500

    Completed the Records lectures

--- Section7/RecordProject/untitled/src/Main.java
+++ Section7/RecordProject/untitled/src/Main.java
@@ -1,7 +1,9 @@
+import java.sql.Struct;
+
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i<=5; i++) {
-            Student s = new Student("S 92300" + i,
+            LPAStudent s = new LPAStudent("S 92300" + i,
                     switch (i) {
                         case 1 -> "Mary";
                         case 2 -> "Carol";
@@ -15,5 +17,15 @@ public class Main {
 
             System.out.println(s);
         }
+
+        Student pojoStudent = new Student("S92340", "Ann", "12/56/2012", "Java Masterclass");
+
+        LPAStudent recordStudent = new LPAStudent("S23940", "Bill", "04/11/1967", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
     }
 }
