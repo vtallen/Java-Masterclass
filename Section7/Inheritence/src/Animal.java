e154ace40fe393d36092989368a3a1e58bd021bc
vtallen <89719939+vtallen@users.noreply.github.com>
2022-12-31 22:17:23 -0500

    Completed Inheritence part 2

---
+++ Section7/Inheritence/src/Animal.java
@@ -0,0 +1,34 @@
+public class Animal {
+
+    private String type;
+    private String size;
+    private double weight;
+
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
+    }
+
+    public Animal() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public void move(String speed) {
+        System.out.println(type + " moves " + speed);
+    }
+
+    public void makeNoise() {
+        System.out.println(type + " made some noise");
+    }
+
+}
---
+++ Section7/Inheritence/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie, "slow");
+
+        Dog retriever = new Dog("Lab", "medium", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "fast");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+
+    }
+}
