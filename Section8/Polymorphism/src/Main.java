ddc07043450b986e7171d069198b996d2c5f3caf
vtallen <vtallen@protonmail.com>
2023-01-05 19:10:34 -0500

    Completed Polymorphism part 1

---
+++ Section8/Polymorphism/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Movie theMovie = new Adventure("Star Wars");
+        theMovie.watchMovie();
+    }
+}
---
+++ Section8/Polymorphism/src/Movie.java
@@ -0,0 +1,49 @@
+public class Movie {
+
+    private String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film");
+    }
+}
+
+class Adventure extends Movie {
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3), "Pleasant Scene", "Scary Music", "Something Bad Happens");
+    }
+}
+
+class Comedy extends Movie {
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3), "Something Funny happens", "Scary Music", "Something Bad Happens");
+    }
+}
+
+class ScienceFiction extends Movie {
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("..%s%n".repeat(3), "Bad aliens do stuff", "Earth is invaded", "Everyone dies");
+    }
+}
