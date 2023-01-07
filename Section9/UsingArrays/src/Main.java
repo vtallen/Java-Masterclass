8fe967822626ab69ca5ae59ce422968f770d6daa
vtallen <vtallen@protonmail.com>
2023-01-07 17:56:05 -0500

    Finished the finding a match lecture

--- Section9/UsingArrays/src/Main.java
+++ Section9/UsingArrays/src/Main.java
@@ -28,6 +28,21 @@ public class Main {
 
         int[] largerArray = Arrays.copyOf(thirdArray, 15);
         System.out.println(Arrays.toString(largerArray));
+
+        String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+        if (Arrays.binarySearch(sArray, "Mark") >= 0) {
+            System.out.println("Found Mark in the list");
+        }
+
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {1, 2, 3, 4, 5};
+
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("Arrays are equal");
+        }
+
     }
 
     private static int[] getRandomArray(int len) {
