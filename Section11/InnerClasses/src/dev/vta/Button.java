77b056f2f6735b24e4aa9c75106fc9c13729c4d9
vtallen <vtallen@protonmail.com>
2023-01-13 14:56:19 -0500

    Completed the inner classes video

---
+++ Section11/InnerClasses/src/dev/vta/Button.java
@@ -0,0 +1,26 @@
+package dev.vta;
+
+public class Button {
+    private String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public void onCLick() {
+        this.onClickListener.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
+}
