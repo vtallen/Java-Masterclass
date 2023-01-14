86418a4f23e777c9722e0ef1fb20d2faeaebf453
vtallen <vtallen@protonmail.com>
2023-01-14 14:39:51 -0500

    Completed Generics Part 2

--- Section12/GenericsPart2/src/dev/vta/Main.java
+++ Section12/GenericsPart2/src/dev/vta/Main.java
@@ -6,10 +6,15 @@ public class Main {
         BaseballPlayer pat = new BaseballPlayer("pat");
         SoccerPlayer beckham = new SoccerPlayer("beckham");
 
-        Team adelaideCrows = new Team("Adelaide Crows");
+        Team<FootballPlayer> adelaideCrows = new Team<FootballPlayer>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
-        adelaideCrows.addPlayer(pat);
-        adelaideCrows.addPlayer(beckham);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
+        Team<SoccerPlayer> soccerTeam = new Team<>("TEST");
+        soccerTeam.addPlayer(beckham);
 
         System.out.println(adelaideCrows.numPlayers());
     }
